7613018ce4289cfecff4e750771a82581cb27be1
ErlindS <107555681+ErlindS@users.noreply.github.com>
2023-05-23 23:31:50 +0200

    Update Vektoren.hpp
    Überflüssige Kommentare wurden entfernt

--- Vektoren.hpp
+++ Vektoren.hpp
@@ -11,19 +11,10 @@ sim::Vector operator-(sim::Vector a, sim::Vector b);
 
 sim::Vector operator/(sim::Vector a, int b);
 
-// D.x*D.x + D.y*D.y < eps * eps
-// sqrt(D.x*D.x + D.y*D.y) < sqrt(eps * eps)
-// sqrt(D.x*D.x + D.y*D.y) < eps
     
 double operator*(sim::Vector a, sim::Vector b);
 
 bool operator==(sim::Vector a, sim::Vector b);
-/*
-sim::Vector orthogonaler_Vector(sim::Vector a){
-    a.x *= -1;
-    return a;
-} */
 
-// double Vector_laenge(sim::Vector a);
 
 
